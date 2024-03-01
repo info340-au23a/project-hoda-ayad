b0f0ce5712dfd11244d77033ba2e7a843af8db19
EmmaFin1 <137982152+EmmaFin1@users.noreply.github.com>
2024-03-01 01:18:59 -0800

    made footer and chat more responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,39 @@ import { Col, Row } from 'reactstrap';
 function Footer(props) {
     return (
       <div className="footer">
-            <Row className="footer-content">
-                <Col>Contact us at:</Col>
-                <Col><a href="mailto:hayad03@uw.edu">hayad03@uw.edu</a></Col>
-                <Col><a href="mailto:yohhn@uw.edu">yohhn@uw.edu</a></Col>
-                <Col><a href="mailto:kchong@uw.edu">kchong@uw.edu</a></Col>
-                <Col><a href="emmafin@uw.edu">emmafin@uw.edu</a></Col>
-            </Row>
+        <div className="large-footer-content">
+          <Row className="footer-content">
+              <Col>Contact us at: </Col>
+              <Col><a href="mailto:hayad03@uw.edu">hayad03@uw.edu</a></Col>
+              <Col><a href="mailto:yohhn@uw.edu">yohhn@uw.edu</a></Col>
+              <Col><a href="mailto:kchong@uw.edu">kchong@uw.edu</a></Col>
+              <Col><a href="emmafin@uw.edu">emmafin@uw.edu</a></Col>
+          </Row>
+        </div>
+
+        <div className="small-footer-content">
+          Contact us at:&nbsp;&nbsp;&nbsp;
+          <Row className="content-small">
+            <Col><a href="mailto:hayad03@uw.edu">hayad03@uw.edu</a></Col>
+            <Col><a href="mailto:yohhn@uw.edu">yohhn@uw.edu</a></Col>
+            <Col><a href="mailto:kchong@uw.edu">kchong@uw.edu</a></Col>
+            <Col><a href="emmafin@uw.edu">emmafin@uw.edu</a></Col>
+          </Row>
+        </div> 
+
+        <div className="extra-small-footer-content">
+          Contact us at:&nbsp;&nbsp;&nbsp;
+          <Row className="content-extra-small">
+            <Col><a href="mailto:hayad03@uw.edu">hayad03@uw.edu</a></Col>
+            <Col><a href="mailto:yohhn@uw.edu">yohhn@uw.edu</a></Col>
+          </Row>
+          <Row className="content-extra-small">
+            <Col><a href="mailto:kchong@uw.edu">kchong@uw.edu</a></Col>
+            <Col><a href="emmafin@uw.edu">emmafin@uw.edu</a></Col>
+          </Row>
+        </div> 
+
+            
       </div>
     );
   }
