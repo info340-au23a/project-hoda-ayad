b078cec285e08ab30c5ff7e084147f6a0542d5c1
EmmaFin1 <137982152+EmmaFin1@users.noreply.github.com>
2024-02-29 22:00:48 -0800

    edited footer, fixed some chat features

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,8 @@ import { Col, Row } from 'reactstrap';
 function Footer(props) {
     return (
       <div className="footer">
-        Contact us at:
             <Row className="footer-content">
+                <Col>Contact us at:</Col>
                 <Col><a href="mailto:hayad03@uw.edu">hayad03@uw.edu</a></Col>
                 <Col><a href="mailto:yohhn@uw.edu">yohhn@uw.edu</a></Col>
                 <Col><a href="mailto:kchong@uw.edu">kchong@uw.edu</a></Col>
