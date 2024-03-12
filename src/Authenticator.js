d9fb31296fe176ca1400fd7f3fe1e8027587b35c
Yohan <yohhhn@uw.edu>
2024-03-12 16:49:26 -0700

    put authenticator in its own file

--- src/Authenticator.js
+++ src/Authenticator.js
@@ -4,20 +4,9 @@ import './css/style.css';
 import { Routes, Route, Outlet,useLocation } from "react-router-dom";
 
 import NavBar from './components/NavBar';
-import Splash from './pages/Splash';
-import Home from './pages/Home';
-import Chat from './pages/Chat';
-import Profile from './pages/Profile';
-import Footer from './components/Footer';
 import {ResetPassword, SetupBasic, SetupEducation, SetupPassword, SetupSkill} from './pages/Setup';
-import Authenticator from './Authenticator';
+import { RequireAuth } from './App';
 
-
-import SAMPLE_POSTS from './sample-data.json';
-import SAMPLE_CHATS from './chat-sample-data.json';
-
-
-/**
 function Authenticator({ signedIn, setSignInCB, children}) {
   function toggleSignIn(toggle) {
     setSignInCB(toggle);
@@ -50,60 +39,10 @@ function Authenticator({ signedIn, setSignInCB, children}) {
         <Route path="set-up-skills" element={<SetupSkill setSkills={setSkills} email={email} password={password} name={name}
           username={username} college={college} major={major} gradDate={gradDate} skills={skills} />} />
         <Route path="reset-password" element={<ResetPassword />} />
+        {children}
       </Routes>
     </div>
   )
 }
-*/
 
-export function RequireAuth({ signedIn, setSignedIn, email, password, setUser, setEmail, setPassword }) {
-  function signIn() {
-    setSignedIn(true);
-  }
-
-  if(!signedIn) {
-    return <Splash signInCB={signIn} email={email} password={password} setUser={setUser} setEmail={setEmail} setPassword={setPassword}  />
-  }
-  else {
-    return <Outlet />
-  }
-}
-
-
-function App() {
-  const samplePosts = SAMPLE_POSTS;
-  const sampleChats = SAMPLE_CHATS;
-
-  const [signedIn, setSignedIn] = useState(false);
-
-  function toggleSignIn(toggle) {
-    setSignedIn(toggle);
-  }
-
-  const location = useLocation();
-  const isFooterVisible = location.pathname !== '/set-up-college' && location.pathname !== '/set-up-password' &&
-    location.pathname !== '/set-up-skills' && location.pathname !== '/set-up-basic' && location.pathname !== '/' &&
-    location.pathname !== '/reset-password';;
-
-  return (
-    <div>
-      <div className="App full-height">
-
-              <Authenticator signedIn={signedIn} setSignInCB={toggleSignIn} >
-
-                  <Route index element={<Home postings={samplePosts} />} />
-                  <Route path="chat" element={<Chat chats={sampleChats} />} />
-                  <Route path="profile" element={<Profile />} />
-
-              </Authenticator>
-
-
-      </div>
-      <footer>
-        {isFooterVisible && <footer><Footer /></footer>}
-      </footer>
-    </div>
-  );
-}
-
-export default App;
+export default Authenticator;
